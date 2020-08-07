bf9575d1946a237ae533c6e47055e20275d64dc5
Milan_Patel <ptlmilan52@gmail.com>
2020-08-07 12:43:35 -0400

    Final Code.

---
+++ README.md
@@ -0,0 +1,14 @@
+## Hangman Game
+Select a letter to figure out a hidden word in a set amount of chances
+
+## Project Specifications
+- Display hangman pole and figure using SVG
+- Generate a random word
+- Display word in UI with correct letters
+- Display wrong letters
+- Show notification when select a letter twice
+- Show popup on win or lose
+- Play again button to reset game
+
+## Code Credit
+Credit: Created while Learning the Javascript from Brad Traversy's Udemy Course: https://www.udemy.com/course/web-projects-with-vanilla-javascript/
---
+++ myscript.js
@@ -0,0 +1,107 @@
+const wrongLetterEl = document.getElementById('wrong-letters');
+const wordEl = document.getElementById('word');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const figureParts = document.querySelectorAll('.figure-part');
+const finalMessage = document.getElementById('final-message');
+const playAgainBtn = document.getElementById('play-again');
+
+
+const words = ["wizard","application","interface","object"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord
+        .split('')
+        .map(
+            letter =>  `<span class="letter">${correctLetters.includes(letter) ? letter : ''}</span>`
+            ).join('')
+        }`;
+
+        const innerWord = wordEl.innerText.replace(/\n/g,'');
+
+        if (innerWord === selectedWord) {
+            finalMessage.innerText = 'Congratulations! You won! 😃';
+            popup.style.display = 'flex';
+        }
+
+}
+
+function displayNotification() {
+    notification.classList.add('show');
+    setTimeout ( () => 
+        notification.classList.remove('show')
+        ,3000);
+}
+
+function updateWrongLettersEl() {   
+    wrongLetterEl.innerHTML = `
+        ${wrongLetters.length === 0 ? '' :`<p>Wrong Letters:</p>`}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`).join(',')}
+    `;
+
+    figureParts.forEach((part,index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+
+    });
+
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost. 😕';
+        popup.style.display = 'flex';
+    }
+
+}
+
+//Event Listeners
+
+document.addEventListener('keydown', e => {
+    if(e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+        if(selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                displayNotification();
+            }
+        } else {
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                displayNotification();
+            }
+        }
+    }
+});
+
+playAgainBtn.addEventListener('click', () => {
+
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none';
+
+});
+
+
+displayWord();
